80bca5042939107b149df6269280194f1d7734a8
Marek Polakowski <marek.polakowski@gmail.com>
2021-07-07 16:16:26 +0200

    setup React

---
+++ client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './src/components/Home';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'client/src'),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        // Conditions:
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react',
+          ],
+        },
+      },
+    ],
   },
 };
